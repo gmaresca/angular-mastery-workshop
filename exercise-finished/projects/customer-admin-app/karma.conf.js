500b7db0582a223937c2baf4c887b7524189c7f2
tomastrajan <tomas.trajan@gmail.com>
2019-10-16 20:08:18 +0200

    feat(finished): fix karma config

--- exercise-finished/projects/customer-admin-app/karma.conf.js
+++ exercise-finished/projects/customer-admin-app/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml', 'text-summary'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
