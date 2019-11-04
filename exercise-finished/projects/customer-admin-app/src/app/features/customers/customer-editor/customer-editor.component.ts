13be2403ba4226d40d8b62980afdc06e474f6862
tomastrajan <tomas.trajan@gmail.com>
2019-11-04 08:04:13 +0100

    feat(finished): add customer editor impl

--- exercise-finished/projects/customer-admin-app/src/app/features/customers/customer-editor/customer-editor.component.ts
+++ exercise-finished/projects/customer-admin-app/src/app/features/customers/customer-editor/customer-editor.component.ts
@@ -1,7 +1,8 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 import { Customer } from '../model/customers';
 import { CustomersBackendService } from '../services/customers-backend.service';
@@ -14,15 +15,40 @@ import { CustomersBackendService } from '../services/customers-backend.service';
 export class CustomerEditorComponent implements OnInit {
   customer: Observable<Customer>;
 
+  customerForm: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     private customersBackendService: CustomersBackendService,
   ) {}
 
   ngOnInit() {
+    this.customerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+    });
+
     this.customer = this.activatedRoute.paramMap.pipe(
       map(paramMap => paramMap.get('id')),
       switchMap(id => this.customersBackendService.get(parseInt(id, 10))),
+      tap(customer => this.customerForm.patchValue({ ...customer })),
     );
+  }
+
+  submit(customer: Customer) {
+    const { name, surname } = this.customerForm.getRawValue();
+    this.customersBackendService
+      .update({
+        ...customer,
+        name,
+        surname,
+      })
+      .subscribe(() => this.router.navigate(['../../'], { relativeTo: this.activatedRoute }));
+  }
+
+  reset(customer: Customer) {
+    this.customerForm.patchValue(customer);
   }
 }
