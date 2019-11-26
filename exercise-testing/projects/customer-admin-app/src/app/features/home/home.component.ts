1532b7fb7b024759c01e7d4fed66aba5058732ea
tomastrajan <tomas.trajan@gmail.com>
2019-11-26 16:25:24 +0100

    chore(exercise-testing): improve demo

--- exercise-testing/projects/customer-admin-app/src/app/features/home/home.component.ts
+++ exercise-testing/projects/customer-admin-app/src/app/features/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ReactiveNotificationService } from '../../core/notification/reactive-notification.service';
+
 @Component({
   selector: 'my-org-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private notificationService: ReactiveNotificationService) {}
 
   ngOnInit() {}
+
+  info() {
+    this.notificationService.info('This is info!');
+  }
+
+  warning() {
+    this.notificationService.warning('This is warning');
+  }
+
+  error() {
+    this.notificationService.error('This is error');
+  }
 }
--- exercise-testing/projects/customer-admin-app/src/app/features/home/home.module.ts
+++ exercise-testing/projects/customer-admin-app/src/app/features/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, MatButtonModule, HomeRoutingModule],
 })
 export class HomeModule {}
