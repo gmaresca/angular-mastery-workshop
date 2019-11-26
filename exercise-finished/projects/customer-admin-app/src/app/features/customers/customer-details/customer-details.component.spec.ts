c8e1ecb70a41e909889cd04e5d830469e6c99d6c
tomastrajan <tomas.trajan@gmail.com>
2019-11-26 13:11:10 +0100

    chore(exercise-finished): add tests

--- exercise-finished/projects/customer-admin-app/src/app/features/customers/customer-details/customer-details.component.spec.ts
+++ exercise-finished/projects/customer-admin-app/src/app/features/customers/customer-details/customer-details.component.spec.ts
@@ -1,25 +1,63 @@
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { Observable, of, asyncScheduler } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 import { SharedModule } from '../../../shared/shared.module';
 
 import { CustomerDetailsComponent } from './customer-details.component';
 import { CustomersBackendService } from '../services/customers-backend.service';
+import { ActivatedRoute } from '@angular/router';
+import { Customer } from '../model/customers';
+
+const MOCK_CUSTOMER = {
+  id: 0,
+  name: 'John',
+  surname: 'Snow',
+  address: {
+    location: 'Castle Black',
+    land: 'The North',
+    continent: 'Westeros',
+  },
+  tags: ['bastard'],
+};
 
 describe('CustomerDetailsComponent', () => {
   let component: CustomerDetailsComponent;
   let fixture: ComponentFixture<CustomerDetailsComponent>;
+  let mockCustomerBackendService: Partial<CustomersBackendService>;
+
+  const getAddTagInput = () => fixture.debugElement.query(By.css('input'));
+  const getAddTagButton = () => fixture.debugElement.query(By.css('button[mat-mini-fab]'));
+  const getRemoveTagButton = tagIndex =>
+    fixture.debugElement.queryAll(By.css('my-org-tag-list > div button'))[tagIndex];
+  const getSpinner = () => fixture.debugElement.query(By.css('mat-spinner '));
+  const getName = () =>
+    fixture.debugElement.query(By.css('h1 span')).nativeElement.textContent.trim();
 
   beforeEach(async(() => {
+    mockCustomerBackendService = {
+      get(id: number): Observable<Customer> {
+        return of({ ...MOCK_CUSTOMER }).pipe(delay(50, asyncScheduler));
+      },
+      update(customer: Customer): Observable<Customer> {
+        return of();
+      },
+    };
+    spyOn(mockCustomerBackendService, 'get').and.callThrough();
+    spyOn(mockCustomerBackendService, 'update').and.callThrough();
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, RouterTestingModule, SharedModule],
       providers: [
+        { provide: CustomersBackendService, useValue: mockCustomerBackendService },
         {
-          provide: CustomersBackendService,
+          provide: ActivatedRoute,
           useValue: {
-            get: () => of({ name: 'test', address: {} }),
+            paramMap: of({
+              get: () => '0',
+            }),
           },
         },
       ],
@@ -35,5 +73,66 @@ describe('CustomerDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('renders spinner while loading', done => {
+    expect(getSpinner()).toBeTruthy();
+
+    setTimeout(() => {
+      fixture.detectChanges();
+      expect(getSpinner()).toBeFalsy();
+
+      done();
+    }, 100);
+  });
+
+  it('renders retrieved customer', done => {
+    setTimeout(() => {
+      fixture.detectChanges();
+      expect(getName()).toBe('John Snow');
+
+      expect(mockCustomerBackendService.get).toHaveBeenCalledTimes(1);
+      expect(mockCustomerBackendService.get).toHaveBeenCalledWith(0);
+
+      done();
+    }, 100);
+  });
+
+  it('makes a backend request when adding tag', done => {
+    setTimeout(() => {
+      fixture.detectChanges();
+
+      getAddTagInput().nativeElement.value = 'some-tag';
+      getAddTagInput().triggerEventHandler('keyup', { target: getAddTagInput().nativeElement });
+      fixture.detectChanges();
+
+      getAddTagButton().nativeElement.click();
+      fixture.detectChanges();
+
+      expect(mockCustomerBackendService.update).toHaveBeenCalledTimes(1);
+      expect(mockCustomerBackendService.update).toHaveBeenCalledWith({
+        ...MOCK_CUSTOMER,
+        tags: ['bastard', 'some-tag'],
+      });
+
+      done();
+    }, 100);
+  });
+
+  it('makes a backend request when removing tag', done => {
+    setTimeout(() => {
+      fixture.detectChanges();
+
+      getRemoveTagButton(0).nativeElement.click();
+      fixture.detectChanges();
+
+      expect(mockCustomerBackendService.update).toHaveBeenCalledTimes(1);
+      expect(mockCustomerBackendService.update).toHaveBeenCalledWith({
+        ...MOCK_CUSTOMER,
+        tags: [],
+      });
+
+      done();
+    }, 100);
   });
 });
